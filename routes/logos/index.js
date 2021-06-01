c1ae53071edc1e687f9932750a0f455af313af07
Manon.icu <gemini0525@foxmail.com>
2021-06-01 16:58:23 +0800

    update logos

---
+++ routes/logos/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const path = require("path")
+
+router.get('/', function (req, res, next) {
+  const logos = fs.readdirSync(path.join(__dirname, '../../public/logos'));
+  const data = logos.map(logo => ({ name: logo.split(".")[0], route: `https://apis-kohl.vercel.app/logos/${logo}` }));
+  res.json({code:0,data});
+});
+
+module.exports = router;
