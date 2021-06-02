64eb2d0e207ed09fe8c9b6ca84477697a76151a5
Manon.icu <gemini0525@foxmail.com>
2021-06-02 09:58:13 +0800

    add convert

--- routes/logos/index.js
+++ routes/logos/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require("fs");
 const path = require("path")
+const sharp = require("sharp")
 
 router.get('/', function (req, res, next) {
   const logos = fs.readdirSync(path.join(__dirname, '../../public/logos'));
@@ -11,5 +12,17 @@ router.get('/', function (req, res, next) {
     data
   });
 });
+
+router.get('/convert', async function (req, res, next) {
+  const { filename,filetype } = req.query;
+  console.log(filename,filetype)
+  const fileBuffer = Buffer.from(fs.readFileSync(path.join(__dirname, `../../public/logos/${filename}.svg`).toString()));
+  if (!fileBuffer) {
+    res.json({ code:1,data:null})
+  }
+  sharp(fileBuffer).toFile(path.join(__dirname, `../../temp/logos/${filename}.${filetype}`), (err, info) => {
+      res.json({code:0,data:`https://apis-kohl.vercel.app/temp/logos/${filename}.${filetype}`})
+  })
+})
 
 module.exports = router;
