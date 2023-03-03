c02a202ab685d99533761b00d509aef9e3872277
pfan <gemini0525@foxmail.com>
2023-03-03 22:10:12 +0800

    fix: request weather data by coordinates

--- controllers/weather/index.js
+++ controllers/weather/index.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const errorHandle = require('../../utils/errorHandle')
 const API_KEY = process.env.OPEN_WEATHER_API_KEY;
+const IP_TOKEN = process.env.IP_INFO_TOKEN;
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 async function getWeatherByCity(req, res) {
     const {city} = req.body
     try {
-        console.log(city,"city")
         const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}`;
         const response = await axios.get(url);
         return res.json(response.data);
@@ -19,8 +19,13 @@ async function getWeatherByCity(req, res) {
 
 async function getWeatherByCoordinates(req,res) {
     try {
-        const {lat, lon} = req.body
-        const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        const locationUrl = `https://ipinfo.io/${ip}?token=${IP_TOKEN}`;
+        const locationResponse = await axios.get(locationUrl);
+        console.log(locationResponse)
+        const { city} = locationResponse.data;
+
+        const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}`;
         const response = await axios.get(url);
         return res.json(response.data);
     }catch (error) {
